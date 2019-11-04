96714684911b90d3bd0c8a28c3cfbeae9e59e3de
chjoguer <cjguerrero_garcia@outlook.com>
2019-11-04 02:05:52 -0500

    Contactanos

--- src/components/Contactanos.js
+++ src/components/Contactanos.js
@@ -8,9 +8,23 @@ class Contactanos extends React.Component {
 
     render(){
         return(
-            <div>
-                Contactanos
-            </div>
+            <div className="App">
+            <body>
+              <div class="row">
+              <form class="form-block my-2 my-lg-0 col-5 mx-auto pt-5">
+               <p> Nombre: <input class="form-control mr-sm-1" type="text"></input></p>	
+               <p> Apellido: <input class="form-control mr-sm-1" type="text"></input></p>	
+               <p>Lugar:    <input  class="form-control mr-sm-1" type="text"></input></p>
+               <p>Origen:   <input class="form-control mr-sm-1" type="text"></input></p>
+               <p>Correo:   <input class="form-control mr-sm-1" type="text"></input> </p>
+               <p>Comentarios</p>
+                <p><textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="6"  placeholder="Escriba sus comentarios"  ></textarea></p>
+                <button class="btn btn-primary offset-3 col-3   " type="submit">Enviar</button>
+                <button class="btn btn-primary offset-1  col-3" type="">   Limpiar</button>
+              </form>
+              </div>    
+            </body>
+          </div>
         );
     }
 }
