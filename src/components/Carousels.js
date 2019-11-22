4f4d4fe6f68844a31113dea38cf5db682eef6b7e
Kenny Camba Torres <kacamba@espol.edu.ec>
2019-11-21 19:48:05 -0500

    Cambio en pantalla de inicio

--- src/components/Carousels.js
+++ src/components/Carousels.js
@@ -6,29 +6,36 @@ import {
   CarouselIndicators,
   CarouselCaption
 } from 'reactstrap';
-import imagen1 from '../assets/img/slides/1.jpg';
-import imagen2 from '../assets/img/slides/2.jpg';
-import imagen3 from '../assets/img/slides/3.jpg';
+import imagen1 from '../assets/img/slides/manglaralto.jpg';
+import imagen2 from '../assets/img/slides/olon.jpg';
+import imagen3 from '../assets/img/slides/playas.jpg';
+import imagen4 from '../assets/img/slides/salinas.jpg';
 import '../assets/css/carousels.css';
 
 const items = [
   {
     src: imagen1,
-    altText: 'Slide 1',
-    caption: 'Slide 1',
-    subtitulo: 'Subtitulo slide 1',
+    altText: 'Manglaralto',
+    caption: 'Manglaralto',
+    subtitulo: 'Estación 1',
   },
   {
     src: imagen2,
-    altText: 'Slide 2',
-    caption: 'Slide 2',
-    subtitulo: 'Subtitulo slide 2',
+    altText: 'Olon',
+    caption: 'Olon',
+    subtitulo: 'Estación 1',
   },
   {
     src: imagen3,
-    altText: 'Slide 3',
-    caption: 'Slide 3',
-    subtitulo: 'Subtitulo slide 3',
+    altText: 'Playas',
+    caption: 'Playas',
+    subtitulo: 'Estación 2',
+  },
+  {
+    src: imagen4,
+    altText: 'Salinas',
+    caption: 'Salinas',
+    subtitulo: 'Estación 3',
   }
 ];
 
